74c21550bc43baf81c35e41ba453e9caea1ad936
Nambo0 <104467398+Nambo0@users.noreply.github.com>
2023-07-23 22:13:15 -0400

    "deactivated_count"

--- rel/hardcode.cpp
+++ rel/hardcode.cpp
@@ -9,23 +9,23 @@ void tick() {
   // 5-4 Spleef
   case 31:
     // Count number of activated green buttons
-    u32 activated_count = 0;
+    u32 deactivated_count = 0;
     for (u32 i = 0; i < mkb::stagedef->coli_header_count; i++) {
       u32 anim_id = mkb::stagedef->coli_header_list[i].anim_group_id;
       if ((anim_id >= 1 && anim_id <= 45) &&
           mkb::itemgroups[i].anim_frame < 1) {
-        activated_count++;
+        deactivated_count++;
       }
     }
     // for (u32 i = 0; i < mkb::stagedef->button_count; i++) {
     //   mkb::StagedefButton button = mkb::stagedef->button_list[i];
     //   if (button.anim_group_id < 1000 && button.playback_state == 1) {
-    //     activated_count++;
+    //     deactivated_count++;
     //   }
     // }
 
     // If all 45 green buttons are activated, open stunt goal (id 69)
-    if (activated_count < 1) {
+    if (deactivated_count < 1) {
       for (u32 i = 0; i < mkb::stagedef->coli_header_count; i++) {
         if (mkb::stagedef->coli_header_list[i].anim_group_id != 69) {
           continue;

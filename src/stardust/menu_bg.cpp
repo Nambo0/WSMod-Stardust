1a2da0f0fbf3ec73f3c52cbec4c6e311b6dcb694
Eucalyptus <moonlightnightlight123@gmail.com>
2023-10-17 19:47:36 -0700

    Full menu BG code

--- src/stardust/menu_bg.cpp
+++ src/stardust/menu_bg.cpp
@@ -3,6 +3,7 @@
 #include "../internal/patch.h"
 #include "internal/tickable.h"
 #include "../mkb/mkb.h"
+#include "utils/ppcutil.h"
 
 namespace menu_bg {
 
@@ -20,11 +21,18 @@ mkb::Ball& ball = mkb::balls[mkb::curr_player_idx];
 void tick() {
     // Decides which BG to display (World of first occupied story save, 0 = w1)
     u8 world = 0;
+    int menu_stages[10] = {381, 382, 383, 384, 385, 386, 387, 388, 389, 390};
     for (u8 i = 0; i < 3; i++){
         if(mkb::storymode_save_files[i].is_valid){
             world = mkb::storymode_save_files[i].current_world;
             break;
         }
+    }
+    patch::write_word(reinterpret_cast<void*>(0x80282c10), PPC_INSTR_LI(PPC_R31, menu_stages[world]));
+    patch::write_word(reinterpret_cast<void*>(0x80282c20), PPC_INSTR_LI(PPC_R31, menu_stages[world]));
+    if (mkb::main_mode == mkb::MD_GAME) {
+        patch::write_word(reinterpret_cast<void*>(0x808fd958), PPC_INSTR_LI(PPC_R3, menu_stages[world]));
+        patch::write_word(reinterpret_cast<void*>(0x808fe7d8), PPC_INSTR_LI(PPC_R3, menu_stages[world]));
     }
     // ball.banana_count = world;
 }

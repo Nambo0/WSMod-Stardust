b8929c359da6dc30d41f23f4378f0705c3642c71
Nambo0 <104467398+Nambo0@users.noreply.github.com>
2023-11-04 02:14:48 -0400

    Hook up monuments to savedata

--- src/stardust/hardcode.cpp
+++ src/stardust/hardcode.cpp
@@ -5,9 +5,10 @@
 #include "../internal/tickable.h"
 #include "../mkb/mkb.h"
 #include "../utils/vecutil.h"
-#include "stardust/achievement.h"
-#include "stardust/validate.h"
-#include "stardust/badge.h"
+#include "../stardust/achievement.h"
+#include "../stardust/validate.h"
+#include "../stardust/badge.h"
+#include "../stardust/savedata.h"
 
 namespace hardcode {
 
@@ -20,59 +21,6 @@ TICKABLE_DEFINITION((
         .tick = tick, ))
 
 static bool entered_wormhole = false;
-
-// Dummy vars & functions for testing purposes
-bool read_bool_from_array(u8* array, u16 slot){
-    u32 array_index = slot / 8;
-    u32 bit_offset = slot % 8;
-    if (array[array_index] & (1 << bit_offset)) return true;
-    else return false;
-}
-void write_bool_to_array(u8* array, u16 slot, bool value_to_write){
-    u32 array_index = slot / 8;
-    u32 bit_offset = slot % 8;
-    if(value_to_write){
-        array[array_index] |= 1 << bit_offset;
-    }
-    else{
-        array[array_index] |= 0 << bit_offset;
-    }
-}
-static u8 savedata[] = {0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0x00,
-                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0x00, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10};
-bool savedata_bitfield_true_in_slot(u16 slot){
-    return read_bool_from_array(savedata, slot);
-}
-bool savedata_bitfield_10_true_from_slot(u16 slot){
-    for(int i = slot; i < slot + 10; i++){
-        if(!read_bool_from_array(savedata, i)) return false;
-    }
-    return true;
-}
-bool savedata_bitfield_100_true_from_slot(u16 slot){
-    for(int i = slot; i < slot + 100; i++){
-        if(!read_bool_from_array(savedata, i)) return false;
-    }
-    return true;
-}
-u8 savedata_best_stellar_rank(){
-    u16 stellar_total = savedata[52] + savedata[53] + savedata[54] + savedata[55] + savedata[56] + savedata[57] + savedata[58] + savedata[59];
-    switch(stellar_total){
-        case 0 ... 99: return 0;
-        case 100 ... 199: return 1;
-        case 200 ... 299: return 2;
-        case 300 ... 399: return 3;
-        case 400 ... 499: return 4;
-    }
-    return 5;
-}
-void test(int num){
-    mkb::balls[mkb::curr_player_idx].banana_count = num;
-}
 
 static patch::Tramp<decltype(&mkb::teleport_through_wormhole)> s_teleport_through_wormhole_tramp;
 
@@ -332,8 +280,7 @@ void tick() {
                     switch(anim_id){
                         // Claimed badges
                         case 0 ... 309: {
-                            if (savedata_bitfield_true_in_slot(anim_id)) {
-                                test(30);
+                            if (savedata::true_in_slot(anim_id)) {
                                 mkb::itemgroups[i].playback_state = 0;
                                 mkb::itemgroups[i].anim_frame = 2;
                             }
@@ -342,15 +289,15 @@ void tick() {
 
                         // Interstellar Rank Medallion
                         // These positions are variable so they all need separate cases
-                        case 400: if (savedata_best_stellar_rank() == 1) mkb::itemgroups[i].playback_state = 0; break;
-                        case 401: if (savedata_best_stellar_rank() == 2) mkb::itemgroups[i].playback_state = 0; break;
-                        case 402: if (savedata_best_stellar_rank() == 3) mkb::itemgroups[i].playback_state = 0; break;
-                        case 403: if (savedata_best_stellar_rank() == 4) mkb::itemgroups[i].playback_state = 0; break;
-                        case 404: if (savedata_best_stellar_rank() == 5) mkb::itemgroups[i].playback_state = 0; break;
+                        case 400: if (savedata::best_stellar_rank() == 1) mkb::itemgroups[i].playback_state = 0; break;
+                        case 401: if (savedata::best_stellar_rank() == 2) mkb::itemgroups[i].playback_state = 0; break;
+                        case 402: if (savedata::best_stellar_rank() == 3) mkb::itemgroups[i].playback_state = 0; break;
+                        case 403: if (savedata::best_stellar_rank() == 4) mkb::itemgroups[i].playback_state = 0; break;
+                        case 404: if (savedata::best_stellar_rank() == 5) mkb::itemgroups[i].playback_state = 0; break;
                         
                         // World sweep statue decorations
                         case 500 ... 519:{
-                            if (savedata_bitfield_10_true_from_slot((anim_id - 500)*10 + 100)) {
+                            if (savedata::consecutive_true_from_slot((anim_id - 500)*10 + 100, 10)) {
                                 mkb::itemgroups[i].playback_state = 0;
                                 mkb::itemgroups[i].anim_frame = 2;
                             }
@@ -359,7 +306,7 @@ void tick() {
 
                         // Story sweep statues
                         case 600: case 601:{
-                            if (savedata_bitfield_100_true_from_slot((anim_id - 600)*100 + 100)) {
+                            if (savedata::consecutive_true_from_slot((anim_id - 600)*100 + 100, 100)) {
                                 mkb::itemgroups[i].playback_state = 0;
                                 mkb::itemgroups[i].anim_frame = 2;
                             }
@@ -410,5 +357,12 @@ void tick() {
         patch::write_word(reinterpret_cast<void*>(0x80339da0), 0x901d004c);
         patch::write_word(reinterpret_cast<void*>(0x80339f14), 0x2c000258);
     }
+
+    // Savedata testing
+    if(pad::button_pressed(mkb::PAD_TRIGGER_Z)){
+        savedata::save();
+        mkb::balls[mkb::curr_player_idx].banana_count = 111;
+    }
+
 }// void tick
 }// namespace hardcode

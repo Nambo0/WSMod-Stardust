6e85e1c53364065505efb0a3db5b45d0b30cd7ea
Nambo0 <104467398+Nambo0@users.noreply.github.com>
2023-11-04 02:12:52 -0400

    Update mkb.h to prepare for cardio/savedata

--- src/mkb/mkb.h
+++ src/mkb/mkb.h
@@ -10,6 +10,30 @@ constexpr GXBool GX_FALSE = 0;
 inline u32 OSRoundUp32B(u32 x) { return (x + 31) & ~31; }
 inline u32 OSRoundDown32B(u32 x) { return x & ~31; }
 
+// Originally #define'd
+enum {
+    CARD_RESULT_READY = 0,
+    CARD_RESULT_BUSY = -1,
+    CARD_RESULT_WRONGDEVICE = -2,
+    CARD_RESULT_NOCARD = -3,
+    CARD_RESULT_NOFILE = -4,
+    CARD_RESULT_IOERROR = -5,
+    CARD_RESULT_BROKEN = -6,
+    CARD_RESULT_EXIST = -7,
+    CARD_RESULT_NOENT = -8,
+    CARD_RESULT_INSSPACE = -9,
+    CARD_RESULT_NOPERM = -10,
+    CARD_RESULT_LIMIT = -11,
+    CARD_RESULT_NAMETOOLONG = -12,
+    CARD_RESULT_ENCODING = -13,
+    CARD_RESULT_CANCELED = -14,
+    CARD_RESULT_FATAL_ERROR = -128,
+};
+typedef s32 CARDResult;
+constexpr s32 CARD_READ_SIZE = 512;
+constexpr s32 CARD_FILENAME_MAX = 32;
+constexpr u32 CARD_WORKAREA_SIZE = 5 * 8 * 1024;
+
 // A few inline GX functions we need
 inline void GXPosition3f32(float x, float y, float z) {
     GXWGFifo.v_f32 = x;
